368105029c188ddc40c5682cd8e665c89de27099
Danivent <dan.vent@laposte.net>
2022-01-09 09:50:10 +0100

    FirstCommit

---
+++ app.js
@@ -0,0 +1,27 @@
+const btns = document.querySelectorAll(".btn-modale");
+const modaleEquipement = document.querySelector(".bloc-modale");
+const fermetureModale = document.querySelector(".fermeture-modale");
+const imgIndex = document.querySelector(".bloc-modale img");
+
+if (window.matchMedia("(min-width: 750px)").matches) {
+    btns.forEach(btn => {
+        btn.addEventListener("click", e => {
+            imgIndex.src = `ressources/img${e.target.getAttribute(
+                "data-index"
+            )}-equip.jpg`;
+            modaleEquipement.classList.add("active-modale");
+        });
+    });
+    modaleEquipement.addEventListener("click", () => {
+        modaleEquipement.classList.remove("active-modale");
+    });
+}
+// Animation nav-bar
+const nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 30) {
+        nav.classList.add("anim-nav");
+    } else {
+        nav.classList.remove("anim-nav");
+    }
+});
